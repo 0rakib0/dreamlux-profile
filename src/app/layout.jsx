ebd5b34941636b410ffcbf8e07ad55b62ddc3c0e
rakib00 <hassanrakibul926@gmail.com>
2026-03-09 00:44:37 +0600

    create components folder and nav and footer components

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import FooterSec from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar></Navbar>
         {children}
+        <FooterSec></FooterSec>
       </body>
     </html>
   );
