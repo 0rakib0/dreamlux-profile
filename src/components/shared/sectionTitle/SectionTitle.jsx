4da37b31c925be20f36a0575d1d736e01e86e9c4
rakib00 <hassanrakibul926@gmail.com>
2026-04-14 01:29:11 +0600

    update title design

--- src/components/shared/sectionTitle/SectionTitle.jsx
+++ src/components/shared/sectionTitle/SectionTitle.jsx
@@ -5,7 +5,7 @@ function SectionTitle({title, subTitle, width}) {
         <div>
             <div className={`mt-12 ${width} mx-auto text-center border border-[#0E6B66] relative`}>
                 <FaCouch className="text-[#0E6B66] text-6xl absolute -top-10 left-1/2 transform -translate-x-1/2 bg-white p-2 rounded-full" />
-                <h1 className='p-4 text-xl font-bold'>{title}</h1>
+                <h1 className='p-4 text-xl font-semibold tracking-wide text-[#0b5a55]'>{title}</h1>
                 <p className='text-[#0E6B66] absolute -bottom-3 left-1/2 transform -translate-x-1/2 bg-white rounded-full'>{subTitle}</p>
             </div>
         </div>
